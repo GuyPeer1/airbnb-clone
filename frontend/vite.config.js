2b1a4d41de0a049d81b3ecc687747e23ae8a5fdf
Guy Peer <peer.g2@gmail.com>
2023-08-08 16:33:30 +0300

    changed vite.config base url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,18 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/public/',
+  base: process.env.NODE_ENV === 'production' ? '/public/' : '/',
   plugins: [react()],
   optimizeDeps: { // 👈 optimizedeps
     esbuildOptions: {
-      target: "esnext", 
+      target: "esnext",
       // Node.js global to browser globalThis
       define: {
         global: 'globalThis'
       },
-      supported: { 
-        bigint: true 
+      supported: {
+        bigint: true
       },
     }
-  }, 
+  },
 });
