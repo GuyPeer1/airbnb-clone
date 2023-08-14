c5554ce9ca25cd7d91e3c373860d61d3b9dc378a
Guy Peer <peer.g2@gmail.com>
2023-08-14 16:58:26 +0300

    new build - fixed vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/public/' : '/',
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: '../frontend/src/main.jsx', // Relative path to your frontend main JS file
+    },
+  },
   plugins: [react()],
   optimizeDeps: { // 👈 optimizedeps
     esbuildOptions: {
